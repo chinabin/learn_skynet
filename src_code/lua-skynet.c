8d5ac0b88a67edfdf90a38fbfa15b4d14c998bf4
tab <754136505@qq.com>
2018-02-18 21:09:42 +0800

    read first version of skynet

--- src_code/lua-skynet.c
+++ src_code/lua-skynet.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+//最终在 _dispatch_message 中被调用
 static void
 _cb(struct skynet_context * context, void * ud, const char * addr, const void * msg, size_t sz_session) {
 	lua_State *L = ud;
@@ -30,12 +31,14 @@ _callback(lua_State *L) {
 		return luaL_error(L, "Init skynet context first");
 	}
 
-	luaL_checktype(L,1,LUA_TFUNCTION);
+	luaL_checktype(L,1,LUA_TFUNCTION);	//确保那些调用形式为 skynet.callback 的函数都是传入一个函数作为参数
 	lua_settop(L,1);
 	lua_rawsetp(L, LUA_REGISTRYINDEX, _cb);
+	//QUESTION: 获取主线程的状态，没懂
 	lua_rawgeti(L, LUA_REGISTRYINDEX, LUA_RIDX_MAINTHREAD);
 	lua_State *gL = lua_tothread(L,-1);
 
+	//设置callback函数以及设置参数
 	skynet_callback(context, gL, _cb);
 
 	return 0;
@@ -101,12 +104,17 @@ luaopen_skynet(lua_State *L) {
 
 	luaL_newlibtable(L,l);
 
+	//把 LUA_REGISTRYINDEX 指向的表 t 中键为 "skynet_context" 的值入栈
+	//获取传入 snlua_init 的 ctx 参数
 	lua_getfield(L, LUA_REGISTRYINDEX, "skynet_context");
 	struct skynet_context * ctx = lua_touserdata(L,-1);
 	if (ctx == NULL) {
 		return luaL_error(L, "Init skynet context first");
 	}
 
+	//上面的 lua_getfield 调用是将 ctx 作为上值压入栈， luaL_setfuncs 函数的最后一个参数
+	//使得所有 luaL_Reg 注册的函数都共享这个 ctx
+	//所有注册的函数第一句都是 struct skynet_context * context = lua_touserdata(L, lua_upvalueindex(1)); 来获取这个 ctx
 	luaL_setfuncs(L,l,1);
 
 	return 1;
--- src_code/service_lua.c
+++ src_code/service_lua.c
@@ -7,9 +7,10 @@
 #include <string.h>
 #include <stdlib.h>
 
+//由 skynet_module_instance_create 调用，返回值作为 snlua_init 的第一个参数
 lua_State *
 snlua_create(void) {
-	return luaL_newstate();
+	return luaL_newstate();		//创建新的 Lua 状态机。
 }
 
 static int
@@ -22,7 +23,9 @@ _load(lua_State *L, char ** filename) {
 int
 snlua_init(lua_State *L, struct skynet_context *ctx, const char * args) {
 	lua_gc(L, LUA_GCSTOP, 0);
-	luaL_openlibs(L);
+	luaL_openlibs(L);		//打开 Lua 标准库
+	//下面两句等价于：找到 LUA_REGISTRYINDEX 索引所在的表t，使得t["skynet_context"] = ctx
+	//将 ctx 参数最终传递给 luaopen_skynet 函数
 	lua_pushlightuserdata(L, ctx);
 	lua_setfield(L, LUA_REGISTRYINDEX, "skynet_context");
 	lua_gc(L, LUA_GCRESTART, 0);
@@ -31,12 +34,14 @@ snlua_init(lua_State *L, struct skynet_context *ctx, const char * args) {
 	char *parm = tmp;
 	strcpy(parm,args);
 
+	//将lua文件加载
 	const char * filename = parm;
 	int r = _load(L, &parm);
 	if (r) {
 		skynet_error(ctx, "lua parser [%s] error : %s", filename, lua_tostring(L,-1));
 		return 1;
 	}
+	//为后面调用lua文件代码做准备：获取参数
 	int n=0;
 	while(parm) {
 		const char * arg = strsep(&parm, " \r\n");
@@ -45,6 +50,10 @@ snlua_init(lua_State *L, struct skynet_context *ctx, const char * args) {
 			++n;
 		}
 	}
+	/*
+		调用lua代码，因为lua代码第一句都是 require "skynet" ，
+		所以将会调用 lua-skynet.c 文件中的 luaopen_skynet 函数
+	*/
 	r = lua_pcall(L,n,0,0);
 	switch (r) {
 	case LUA_OK:
