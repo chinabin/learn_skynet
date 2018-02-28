e46501d5c7ef8b2dc8beedb4cb9241e58149e1ad
tanbin <tanbin@hnwhkj.com.cn>
2018-02-28 14:10:04 +0800

    read first version of skynet

--- src_code/gate/mread.c
+++ src_code/gate/mread.c
@@ -22,20 +22,20 @@
 
 #define SOCKET_INVALID 0
 #define SOCKET_CLOSED 1
-//suspend状态表示，等待接收数据
+// suspend 状态表示，等待接收数据
 #define SOCKET_SUSPEND 2
-//read状态表示，数据存在于缓存中，可以直接获取(调用buffer_read)
+// read 状态表示，数据存在于缓存中，可以直接获取(调用 buffer read )
 #define SOCKET_READ 3
-//pollin状态表示有数据可以接收(调用socket recv)
+// pollin 状态表示有数据可以接收(调用 socket recv )
 #define SOCKET_POLLIN 4
 
 #define SOCKET_ALIVE	SOCKET_SUSPEND
 
 struct socket {
-	int fd;		//标识
+	int fd;		// 没有使用之前表示数组索引，使用之后表示 socket 句柄
 	/*
-	node 存放了一个链表,是不同时期收到的数据流.
-	如果外部请求的数据块不连续, 就重新申请一块连续空间 temp ,复制过去返回.
+	 node 存放了一个链表，是不同时期收到的数据流。
+	 如果外部请求的数据块不连续, 就重新申请一块连续空间 temp ,复制过去返回。
 	*/
 	struct ringbuffer_block * node;
 	struct ringbuffer_block * temp;
@@ -43,22 +43,21 @@ struct socket {
 };
 
 struct mread_pool {
-	int listen_fd;		//监听句柄
-	int epoll_fd;		//epoll句柄
-	int max_connection;	//最大连接数
-	int closed;			//连接已断开但是其对应资源还未清理完全(腾出socket)的数目
-	int active;			//当前激活的连接的索引，默认为-1
-	int skip;			//一次或者连续多次pull到的数据大小
-	struct socket * sockets;	//指向所有socket结构
-	struct socket * free_socket;	//指向当前可用的空闲socket结构
+	int listen_fd;		// 监听句柄
+	int epoll_fd;		// epoll 句柄
+	int max_connection;	// 连接数上限，同时也是最大同时连接数
+	int closed;			// 连接已断开但是其对应资源还未清理完全(腾出 socket 结构体)的数目
+	int active;			// 当前激活的连接的索引，默认为 -1
+	int skip;			// 一次或者连续多次 pull 到的数据大小
+	struct socket * sockets;	// 指向所有 socket 结构
+	struct socket * free_socket;	// 指向当前可用的空闲 socket 结构
 	struct map * socket_hash;
-	int queue_len;		//事件数目
-	int queue_head;		//事件索引，取值为[0, queue_len - 1]
+	int queue_len;		// 事件数目
+	int queue_head;		// 事件索引，取值为[0, queue_len - 1]
 	struct epoll_event ev[READQUEUE];
 	struct ringbuffer * rb;
 };
 
-//创建max个socket结构并设置fd，返回socket结构数组
 static struct socket *
 _create_sockets(int max) {
 	int i;
@@ -73,7 +72,6 @@ _create_sockets(int max) {
 	return s;
 }
 
-//创建rb并返回rb
 static struct ringbuffer *
 _create_rb(int size) {
 	size = (size + 3) & ~3;
@@ -85,29 +83,26 @@ _create_rb(int size) {
 	return rb;
 }
 
-//删除rb
 static void
 _release_rb(struct ringbuffer * rb) {
 	ringbuffer_delete(rb);
 }
 
-//设置流为非阻塞型
 static int
 _set_nonblocking(int fd)
 {
-	//F_GETFL 取得文件描述符状态旗标，此旗标为open()的参数flags。
+	// F_GETFL 取得文件描述符状态标志。
     int flag = fcntl(fd, F_GETFL, 0);
     if ( -1 == flag ) {
         return -1;
     }
 
-	//把一个套接字设置为非阻塞型
+	// 把一个套接字设置为非阻塞型
     return fcntl(fd, F_SETFL, flag | O_NONBLOCK);
 }
 
 struct mread_pool *
 mread_create(int port , int max , int buffer_size) {
-	//创建一个监听套接字，并且设置为非阻塞
 	int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (listen_fd == -1) {
 		return NULL;
@@ -118,8 +113,8 @@ mread_create(int port , int max , int buffer_size) {
 
 	int reuse = 1;
 	/*
-	SOL_SOCKET表示设置的层级，此参数决定后面参数的含义
-	SO_REUSEADDR，打开或关闭地址复用功能
+	 SOL_SOCKET表示设置的层级，此参数决定后面参数的含义
+	 SO_REUSEADDR，打开或关闭地址复用功能
 	*/
 	setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(int));
 
@@ -138,17 +133,22 @@ mread_create(int port , int max , int buffer_size) {
 		return NULL;
 	}
 
-	//epoll_create告诉内核这个监听的数目，要多一个是因为epoll句柄创建好之后，自身会占据一个fd值。所以最后切记close(epoll_fd);
+	/*
+	 epoll_create 告诉内核监听的 socket fd 数目，要多一个是因为 epoll 句柄创建好之后，
+	 自身会占据一个 fd 值。所以最后切记 close(epoll_fd); 
+	 它其实是在内核申请一空间，用来存放你想关注的 socket fd 上是否发生以及发生了什么事件。
+	 size 就是你在这个 epoll fd 上能关注的最大 socket fd 数。随你定好了，只要你有空间。
+	*/
 	int epoll_fd = epoll_create(max + 1);
 	if (epoll_fd == -1) {
 		close(listen_fd);
 		return NULL;
 	}
 	struct epoll_event ev;
-	ev.events = EPOLLIN;	// EPOLLIN: 触发该事件，表示对应的文件描述符上有可读数据。(包括对端SOCKET正常关闭)；
+	ev.events = EPOLLIN;	// EPOLLIN: 触发该事件，表示对应的文件描述符上有可读数据。(包括对端 SOCKET 正常关闭)；
 	ev.data.fd = listen_fd;
-	//epoll_ctl是epoll的事件注册函数
-	//EPOLL_CTL_ADD：       注册新的fd
+	// epoll_ctl 是 epoll 的事件注册函数
+	// EPOLL_CTL_ADD: 注册新的 fd
 	if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, listen_fd, &ev) == -1) {
 		close(listen_fd);
 		close(epoll_fd);
@@ -184,7 +184,7 @@ mread_close(struct mread_pool *self) {
 	struct socket * s = self->sockets;
 	for (i=0;i<self->max_connection;i++) {
 		if (s[i].status >= SOCKET_ALIVE) {
-			close(s[i].fd);	//释放连接
+			close(s[i].fd);
 		}
 	}
 	free(s);
@@ -197,13 +197,17 @@ mread_close(struct mread_pool *self) {
 	free(self);
 }
 
+/*
+ 在 timerout 时间内，查看 epoll 上是否有事件发生。
+ 如果有，返回事件数目 n 。对应事件会存储在 self->ev 的前 n 个位置。
+*/
 static int
 _read_queue(struct mread_pool * self, int timeout) {
 	self->queue_head = 0;
 	/*
 	 Epoll 最大的优点就在于它只管你“活跃”的连接。
 	*/
-	//epoll_wait该函数用于轮询I/O事件的发生
+	// epoll_wait 用于轮询 I/O 事件的发生
 	int n = epoll_wait(self->epoll_fd , self->ev, READQUEUE, timeout);
 	if (n == -1) {
 		self->queue_len = 0;
@@ -215,8 +219,6 @@ _read_queue(struct mread_pool * self, int timeout) {
 
 /*
 参考例子：https://www.cnblogs.com/fnlingnzb-learner/p/5835573.html
-epoll_wait返回epoll事件数目，假设返回n，events前n个元素就是发生事件的元素
-所以这里的调用合理
 */
 inline static int
 _read_one(struct mread_pool * self) {
@@ -226,6 +228,7 @@ _read_one(struct mread_pool * self) {
 	return self->ev[self->queue_head ++].data.fd;
 }
 
+// 获取一个空闲的 socket 结构体
 static struct socket *
 _alloc_socket(struct mread_pool * self) {
 	if (self->free_socket == NULL) {
@@ -241,6 +244,7 @@ _alloc_socket(struct mread_pool * self) {
 	return s;
 }
 
+// 添加客户端连接， fd 是 accept 返回的 socket 句柄
 static void
 _add_client(struct mread_pool * self, int fd) {
 	struct socket * s = _alloc_socket(self);
@@ -263,6 +267,7 @@ _add_client(struct mread_pool * self, int fd) {
 	map_insert(self->socket_hash , fd , id);
 }
 
+// 返回一个处于关闭状态的 socket 数组索引
 static int
 _report_closed(struct mread_pool * self) {
 	int i;
@@ -276,32 +281,33 @@ _report_closed(struct mread_pool * self) {
 	return -1;
 }
 
-//有数据可读取，或者有清理操作要完成
+// 根据状态设置好 self->active ，以及对应的 socket 结构体的 status 状态。
+// 方便 mread_pull 进行操作。
 int
 mread_poll(struct mread_pool * self , int timeout) {
 	self->skip = 0;
 	if (self->active >= 0) {
 		struct socket * s = &self->sockets[self->active];
-		if (s->status == SOCKET_READ) {
+		if (s->status == SOCKET_READ) {		// 缓存中有数据可以读取
 			return self->active;
 		}
 	}
 	if (self->closed > 0 ) {
-		return _report_closed(self);	//有断开的连接对应的资源清理
+		return _report_closed(self);	// 有断开的连接对应的资源清理
 	}
-	if (self->queue_head >= self->queue_len) {
+	if (self->queue_head >= self->queue_len) {	// 当前事件处理完了，需要新的事件
 		if (_read_queue(self, timeout) == -1) {
 			self->active = -1;
 			return -1;
 		}
 	}
 	for (;;) {
-		int fd = _read_one(self);
+		int fd = _read_one(self);	// 阻塞，无事件
 		if (fd == -1) {
 			self->active = -1;
 			return -1;
 		}
-		if (fd == self->listen_fd) { 	//表示有新的连接
+		if (fd == self->listen_fd) { 	// 表示有新的连接
 			struct sockaddr_in remote_addr;
 			socklen_t len = sizeof(struct sockaddr_in);
 			int client_fd = accept(self->listen_fd , (struct sockaddr *)&remote_addr ,  &len);
@@ -309,7 +315,7 @@ mread_poll(struct mread_pool * self , int timeout) {
 //				printf("MREAD connect %s:%u (fd=%d)\n",inet_ntoa(remote_addr.sin_addr),ntohs(remote_addr.sin_port), client_fd);
 				_add_client(self, client_fd);
 			}
-		} else { 	//表示fd上有事件发生
+		} else { 	// 表示有数据读取事件发生
 			int index = map_search(self->socket_hash , fd);
 			if (index >= 0) {
 				self->active = index;
@@ -321,12 +327,13 @@ mread_poll(struct mread_pool * self , int timeout) {
 	}
 }
 
-//返回index索引的socket句柄
+// 返回 index 对应的 socket 结构体的 fd 成员，即 socket 句柄
 int
 mread_socket(struct mread_pool * self, int index) {
 	return self->sockets[index].fd;
 }
 
+// 将数据块链接到 s->node 
 static void
 _link_node(struct ringbuffer * rb, int id, struct socket * s , struct ringbuffer_block * blk) {
 	if (s->node) {
@@ -337,13 +344,13 @@ _link_node(struct ringbuffer * rb, int id, struct socket * s , struct ringbuffer
 	}
 }
 
-//关闭客户端连接
+// 关闭客户端连接
 void
 mread_close_client(struct mread_pool * self, int id) {
 	struct socket * s = &self->sockets[id];
 	s->status = SOCKET_CLOSED;
-	//如果不是在_close_active中调用此函数的时候
-	//collect函数已经将对应的blk标记为废弃了
+	// 如果不是在 _close_active 中调用此函数的时候
+	// collect 函数已经将对应的 blk 标记为废弃了
 	s->node = NULL;
 	s->temp = NULL;
 	close(s->fd);
@@ -353,7 +360,7 @@ mread_close_client(struct mread_pool * self, int id) {
 	++self->closed;
 }
 
-//关闭当前激活的连接，并使对应的blk废弃
+// 关闭当前激活的连接，并使对应的 blk 废弃
 static void
 _close_active(struct mread_pool * self) {
 	int id = self->active;
@@ -363,7 +370,7 @@ _close_active(struct mread_pool * self) {
 	mread_close_client(self, id);
 }
 
-//从self->node中读取size大小数据
+// 从 self->node 中读取 size 大小数据
 static char *
 _ringbuffer_read(struct mread_pool * self, int *size) {
 	struct socket * s = &self->sockets[self->active];
@@ -380,25 +387,25 @@ _ringbuffer_read(struct mread_pool * self, int *size) {
 //获取数据
 void * 
 mread_pull(struct mread_pool * self , int size) {
-	if (self->active == -1) {	//表示无数据可以获取
+	if (self->active == -1) {	// 表示无事件发生
 		return NULL;
 	}
 	struct socket *s = &self->sockets[self->active];
 	int rd_size = size;
 	char * buffer = _ringbuffer_read(self, &rd_size);
 	if (buffer) {
-		//这里应该可以assert(s->status == SOCKET_READ)
+		// 这里应该可以 assert(s->status == SOCKET_READ)
 		self->skip += size;
 		return buffer;
 	}
 	/*
-	else
-	表示self->node中的数据不够
+	 else
+	 表示self->node中的数据不够
 	*/
 	switch (s->status) {
 	case SOCKET_READ:
 		s->status = SOCKET_SUSPEND;
-	case SOCKET_CLOSED:		//客户端已经断开连接，无需再接收数据，所以返回NULL，之后调用yield函数会腾出socket
+	case SOCKET_CLOSED:		// 客户端已经断开连接，无需再接收数据，所以返回 NULL ，之后调用 yield 函数会腾出socket
 	case SOCKET_SUSPEND:
 		return NULL;
 	default:
@@ -496,7 +503,7 @@ mread_yield(struct mread_pool * self) {
 	struct socket *s = &self->sockets[self->active];
 	ringbuffer_free(self->rb , s->temp);
 	s->temp = NULL;
-	if (s->status == SOCKET_CLOSED && s->node == NULL) {	//客户端已经断开连接，腾出socket
+	if (s->status == SOCKET_CLOSED && s->node == NULL) {	// 客户端已经断开连接，腾出 socket
 		--self->closed;
 		s->status = SOCKET_INVALID;
 		map_erase(self->socket_hash , s->fd);
@@ -521,7 +528,7 @@ mread_closed(struct mread_pool * self) {
 		return 0;
 	}
 	struct socket * s = &self->sockets[self->active];
-	if (s->status == SOCKET_CLOSED && s->node == NULL) {	//如果客户端已经断开连接则回收资源
+	if (s->status == SOCKET_CLOSED && s->node == NULL) {	// 如果客户端已经断开连接则回收资源
 		mread_yield(self);
 		return 1;
 	}
--- src_code/gate/mread.h
+++ src_code/gate/mread.h
@@ -3,21 +3,25 @@
 
 struct mread_pool;
 
-//云风设计思路：https://blog.codingnow.com/2012/04/mread.html
-//仓库地址：https://github.com/cloudwu/mread
+// 云风设计思路： https://blog.codingnow.com/2012/04/mread.html
+// 仓库地址： https://github.com/cloudwu/mread
 
- //创建和删除mread
+/*
+ port: 端口
+ max: 连接数上限值，同时也是同时处理的最大连接数
+ buffer: 缓存大小
+*/
 struct mread_pool * mread_create(int port , int max , int buffer);
 void mread_close(struct mread_pool *m);
 
-//查询现在的状态，看到底发生了什么事，并做出相应的操作
+// 查询现在的状态，看到底发生了什么事，并做出相应的操作
 int mread_poll(struct mread_pool *m , int timeout);
 void * mread_pull(struct mread_pool *m , int size);
 void mread_yield(struct mread_pool *m);
 int mread_closed(struct mread_pool *m);
-//关闭客户端
+// 关闭客户端
 void mread_close_client(struct mread_pool *m, int id);
-//返回index索引的socket句柄
+// 返回index索引的 socket 句柄
 int mread_socket(struct mread_pool *m , int index);
 
 #endif
