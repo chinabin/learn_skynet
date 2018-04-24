5fcc2dd7a00017f3ef25be54e8a819ca00506a1f
tanbin <tanbin@hnwhkj.com.cn>
2018-04-24 08:39:09 +0800

    read code

--- src_code/skynet.lua
+++ src_code/skynet.lua
@@ -5,9 +5,14 @@ local coroutine = coroutine
 local assert = assert
 
 local skynet = {}
-local session_id_coroutine = {}
-local session_coroutine_id = {}
-local session_coroutine_address = {}
+local session_id_coroutine = {}			-- session -> co
+local session_coroutine_id = {}			-- co -> session
+local session_coroutine_address = {}	-- co -> addr
+
+--[[
+因为 coroutine.resume 都是在 suspend 中调用，所以 coroutine.yield 之后
+必定回到 suspend 中。
+--]]
 
 local function suspend(co, result, command, param)
 	assert(result, command)
@@ -34,33 +39,56 @@ function skynet.timeout(ti, func, ...)
 	suspend(co, coroutine.resume(co, ...))
 end
 
+--[[
+等价于 skynet.sleep(0)
+--]]
 function skynet.yield()
 	local session = c.command("TIMEOUT","0")
 	coroutine.yield("SLEEP", tonumber(session))
 end
 
+--[[
+新建一个定时器消息在 ti 时间后执行，之后将
+session co 对保存
+--]]
 function skynet.sleep(ti)
 	local session = c.command("TIMEOUT",tostring(ti))
 	assert(session)
 	coroutine.yield("SLEEP", tonumber(session))
 end
 
+--[[
+ 注册服务名字，如果 name 是以 "." 开始的，则表示节点内注册
+ 否则表示注册全局服务名
+--]]
 function skynet.register(name)
 	return c.command("REG", name)
 end
 
+--[[
+ 返回服务编号的十六进制字符串
+--]]
 function skynet.self()
 	return c.command("REG")
 end
 
+--[[
+ 加载新的服务
+--]]
 function skynet.launch(...)
 	return c.command("LAUNCH", table.concat({...}," "))
 end
 
+--[[
+ 返回系统开机到现在的时间，单位是 10 毫秒
+--]]
 function skynet.now()
 	return tonumber(c.command("NOW"))
 end
 
+--[[
+ 服务销毁
+--]]
 function skynet.exit()
 	c.command("EXIT")
 end
